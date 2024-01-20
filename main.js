29531dae37060d7b53f932df1c2eacb98172b413
Purple-Hat-Coder <155189301+Purple-Hat-Coder@users.noreply.github.com>
2024-01-20 13:46:08 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    canvas.position(660, 200);
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
